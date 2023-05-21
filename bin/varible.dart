624ad0d7ed84a1fd2e4c3f0ae197d4cd0cb8904f
kvjas <Kvjasna9@gmail.com>
2023-05-22 00:41:08 +0530

    commit

---
+++ bin/varible.dart
@@ -0,0 +1,11 @@
+void main(){
+  String name="jasna";
+  int age=21;
+  double mark=8.8;
+  String course="flutter";
+  print("My name is $name");
+  print("age : $age");
+  print("course : $course");
+  print("Mark = $mark");
+  
+}
