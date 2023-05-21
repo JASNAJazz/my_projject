9919a1cf7975d6f427ebc34793f4c1bb3bc5e912
kvjas <Kvjasna9@gmail.com>
2023-05-22 01:29:50 +0530

    commit

--- bin/new dart.dart
+++ bin/new dart.dart
@@ -1,14 +1,23 @@
 class My_class{
-  String name="jasna";
-  int age=21;
-  double mark=9.8;
+  String ? name;
+  int ? age;
+  double ? mark;
   static String course ="flutter";
 
 }
 void main(){
   My_class obj=My_class();
-  print("My name ${obj.name}");
-  print("My age ${obj.age}");
-  print("My mark ${obj.mark}");
+  print("student 1 details");
+  print("My name ${obj.name="jasna"}");
+  print("My age ${obj.age=21}");
+  print("My mark ${obj.mark=9.8}");
+  print("My course ${My_class.course}");
+
+  My_class obj1=My_class();
+  print("    ");
+  print("student 2 details");
+  print("My name ${obj1.name="nishi"}");
+  print("My age ${obj1.age=22}");
+  print("My mark ${obj1.mark=8.8}");
   print("My course ${My_class.course}");
 }
